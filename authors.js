41188503f4b577bbee3de7a0e67e383ea47350fb
Feredev <hector1567xd@gmail.com>
2023-11-12 22:27:55 -0400

    Added autor collection

--- authors.js
+++ authors.js
@@ -9,22 +9,85 @@ export default class AuthorsRepository {
       await this.db.createCollection(this.collectionName, {
           validator: {
               $jsonSchema: {
-              bsonType: 'object',
-              required: ['email', 'contraseña'],
-              properties: {
-                  email: {
-                      bsonType: 'string',
-                      description: 'debe ser una cadena y es obligatorio',
+                  bsonType: 'object',
+                  required: ['nombre', 'seudonimo', 'nacimiento', 'sexo', 'padres', 'hijos', 'hermanos'],
+                  properties: {
+                      nombre: {
+                          bsonType: 'string',
+                          description: 'debe ser una cadena y es obligatorio',
+                      },
+                      seudonimo: {
+                          bsonType: 'string',
+                          description: 'debe ser una cadena y es obligatorio',
+                      },
+                      nacimiento: {
+                          bsonType: 'date',
+                          description: 'debe ser una fecha y es obligatorio',
+                      },
+                      fallecimiento: {
+                          bsonType: ['date', 'null'], // Puede ser una fecha o nulo
+                          description: 'debe ser una fecha o nulo',
+                      },
+                      sexo: {
+                          enum: ['M', 'F', 'O'], // 'M' para masculino, 'F' para femenino, 'O' para otro
+                          description: 'debe ser "M", "F" o "O" y es obligatorio',
+                      },
+                      padres: {
+                          bsonType: 'array',
+                          items: {
+                              bsonType: 'object',
+                              required: ['nombre', 'nacimiento'],
+                              properties: {
+                                  nombre: {
+                                      bsonType: 'string',
+                                      description: 'debe ser una cadena y es obligatorio',
+                                  },
+                                  nacimiento: {
+                                      bsonType: 'date',
+                                      description: 'debe ser una fecha y es obligatorio',
+                                  },
+                              },
+                          },
+                      },
+                      hijos: {
+                          bsonType: 'array',
+                          items: {
+                              bsonType: 'object',
+                              required: ['nombre', 'nacimiento'],
+                              properties: {
+                                  nombre: {
+                                      bsonType: 'string',
+                                      description: 'debe ser una cadena y es obligatorio',
+                                  },
+                                  nacimiento: {
+                                      bsonType: 'date',
+                                      description: 'debe ser una fecha y es obligatorio',
+                                  },
+                              },
+                          },
+                      },
+                      hermanos: {
+                          bsonType: 'array',
+                          items: {
+                              bsonType: 'object',
+                              required: ['nombre', 'nacimiento'],
+                              properties: {
+                                  nombre: {
+                                      bsonType: 'string',
+                                      description: 'debe ser una cadena y es obligatorio',
+                                  },
+                                  nacimiento: {
+                                      bsonType: 'date',
+                                      description: 'debe ser una fecha y es obligatorio',
+                                  },
+                              },
+                          },
+                      },
                   },
-                  contraseña: {
-                      bsonType: 'string',
-                      description: 'debe ser una cadena y es obligatorio',
-                  },
-              },
               },
           },
       });
     
-    await this.db.collection(this.collectionName).createIndex({ email: 1 }, { unique: true });
+    await this.db.collection(this.collectionName).createIndex({ nombre: 1 }, { unique: true });
   }
 }
